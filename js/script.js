2ace33fdd1e5095fd93cd85bf1ff695e2908d7df
Oloo Julius <juliusolooolero@gmail.com>
2020-02-18 08:33:26 +0300

    It will add const var on names given to males and females

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Akan names from Sunday
+
+const akanMales = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+]
+
+const akanFemales = [
+    "Akosua",
+    "Adwoa",
+    "Abenna",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+]
